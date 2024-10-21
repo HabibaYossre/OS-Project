9f5a8ed331daa8c042bec59414da3e25988de4e2
salama914 <samakhled@gmail.com>
2024-10-21 14:39:28 +0300

    paramvalidation syscall

--- kern/trap/syscall.c
+++ kern/trap/syscall.c
@@ -315,6 +315,10 @@ void sys_free_user_mem(uint32 virtual_address, uint32 size)
 void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 {
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
+if(virtual_address<USER_HEAP_START&&virtual_address+size>USER_HEAP_MAX){//sama:user_heap_max is a constant pointer not an integer
+	env_exit();
+}
+else if((uint32*)virtual_address==NULL){env_exit();}
 
 	allocate_user_mem(cur_env, virtual_address, size);
 	return;
@@ -323,7 +327,10 @@ void sys_allocate_user_mem(uint32 virtual_address, uint32 size)
 void sys_allocate_chunk(uint32 virtual_address, uint32 size, uint32 perms)
 {
 	//TODO: [PROJECT'24.MS1 - #03] [2] SYSTEM CALLS - Params Validation
-
+	if(virtual_address<USER_HEAP_START&&virtual_address+size>USER_HEAP_MAX){//sama:user_heap_max is a constant pointer not an integer
+		env_exit();
+	}
+	else if((uint32*)virtual_address==NULL){env_exit();}
 	allocate_chunk(cur_env->env_page_directory, virtual_address, size, perms);
 	return;
 }
